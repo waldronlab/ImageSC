11162028854875dc89c66175d98177a0a72d9ec2
LiNk-NY <mramos09@gmail.com>
2016-05-10 13:01:51 -0400

    prepare script for R CMD BATCH

--- R/imageProc.R
+++ R/imageProc.R
@@ -1,22 +1,30 @@
 library(CRImage)
 library(EBImage)
 
-fpatt <- "*.png"
+args <- (commandArgs(TRUE))
+if (all(names(args) == c("maxShape", "minShape", "failureRegion", "numWindows", "pattern"))) {
+stop("supplied arguments must match segmentImage arguments and image search pattern\n",
+'\tc("maxShape", "minShape", "failureRegion", "numWindows", "pattern")')
+}
+args[1:4] <- lapply(args[1:4], as.numeric)
+
+ImgPattern <- args$pattern
 folders <- list.dirs(recursive = FALSE,
-					 path = file.path("rawdata"),
-					 full.names = FALSE)
+                     path = file.path("rawdata"),
+                     full.names = FALSE)
 
 for (i in folders) {
-	pngfiles <- dir(file.path("rawdata", folders[i]),
-					pattern = fpatt, full.names = TRUE)
-	featList <- lapply(pngfiles, function(img) {
-					   segmentImage(
-									filename = img,
-									maxShape = 800,
-									minShape = 40,
-									failureRegion = 2000,
-									threshold = "otsu",
-									numWindows = 4)$features
-					})
+    imgFiles <- dir(file.path("rawdata", folders[i]),
+                    pattern = ImgPattern, full.names = TRUE)
+    featList <- lapply(imgFiles, function(img) {
+                       segmentImage(
+                                    filename = img,
+                                    maxShape = args$maxShape,
+                                    minShape = args$minShape,
+                                    failureRegion = args$failureRegion,
+                                    threshold = "otsu",
+                                    numWindows = args$numWindows)$features
+                    })
+    saveRDS(featList, file = paste0(folders[i], "_features", ".rds"), compress = "bzip2")
 }
 
--- TileSVS.sh
+++ TileSVS.sh
@@ -3,27 +3,27 @@
 shopt -s nullglob
 FILES=(*.svs)
 if [ ${#FILES[@]} -eq 0 ]; then 
-	echo "no svs files found"
+    echo "no svs files found"
 else
-	for i in "${FILES[@]}"
-	do 
-		FOLDER=$(echo ${i} | cut -d. -f1)
-		if [ -d ${FOLDER} ]; then
-			echo "${FOLDER} exists"
-		else
-			mkdir ${FOLDER}
-			echo "$i -> ${FOLDER}"
-			vips dzsave ${i} ${FOLDER}/ --suffix ${2} --tile-size ${1} --depth 1 
-			cd ./${FOLDER}/${FOLDER}_files/0
-			shopt -s dotglob
-			mv -- * ../..
-			shopt -u dotglob
-			cd ../..
-			rm -r ./${FOLDER}_files
-			mv *.dzi ../
-			cd ..
-		fi
-	done
+    for i in "${FILES[@]}"
+    do
+        FOLDER=$(echo ${i} | cut -d. -f1)
+        if [ -d ${FOLDER} ]; then
+            echo "${FOLDER} exists"
+        else
+            mkdir ${FOLDER}
+            echo "$i -> ${FOLDER}"
+            vips dzsave ${i} ${FOLDER}/ --suffix ${2} --tile-size ${1} --depth 1
+            cd ./${FOLDER}/${FOLDER}_files/0
+            shopt -s dotglob
+            mv -- * ../..
+            shopt -u dotglob
+            cd ../..
+            rm -r ./${FOLDER}_files
+            mv *.dzi ../
+            cd ..
+        fi
+    done
 fi
 echo "done"
 shopt -u nullglob
