8c93a131865a08331e994693ad2e3f2ab658884e
LiNk-NY <mramos09@gmail.com>
2016-05-12 18:29:03 -0400

    add overarching script

---
+++ globalScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+R CMD BATCH --no-save --no-restore '--args disease="prad"' R/dlImages.R downloadLogs.out
+
+./TileSVS.sh 400 .png
+
+R CMD BATCH --no-save --no-restore '--args maxShape=800 minShape=40 failureRegion=2000 numWindows=4 pattern="\\.png" disease="prad"' R/imageProc.R segments.out 
+
+echo "done"
