8c05fdd8585a2056101a1dd8e92e48010ec13cef
LiNk-NY <mramos09@gmail.com>
2016-12-08 18:41:37 -0500

    add exec folder and include basic image manipulation commands

--- SpliceImage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-convert $1 -crop $2@ +repage +adjoin $1_$2@_%d.jpg
-echo done
---
+++ exec/imageManip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# to convert an SVS image
+# convert '$1[3]' $1-1.png
+
+# to display an image
+display '$1[3]'
