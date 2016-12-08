457e36cb15ec7fa9a58bc408c7b159ef4dae9fd9
LiNk-NY <mramos09@gmail.com>
2016-12-08 18:58:45 -0500

    add identify command

--- exec/imageManip.sh
+++ exec/imageManip.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+# Arguments
+# 1. An SVS file
+
+# This script shows you things you can do to SVS images.
+# The SVS file type is a pyramidal (multipage) TIFF file. 
+
+# See list contents of an SVS image
+identify '$1'
 
 # to convert an SVS image
-# convert '$1[3]' $1-1.png
+convert '$1[3]' $1-1.png
 
 # to display an image
 display '$1[3]'
